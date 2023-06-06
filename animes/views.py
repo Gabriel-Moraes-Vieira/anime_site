aa14b8a47335062ce823f40d12698e6ce540d881
Gabriel-Moraes-Vieira <gabrieljooj766@gmail.com>
2023-06-06 08:30:07 -0300

    busca-update

--- animes/views.py
+++ animes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def busca(request):
+    return render(request, 'busca.html')
---
+++ site_anime_projeto/static/js/scripts.js
@@ -0,0 +1,13 @@
+function AbrirNav(){
+    document.getElementById("hidden-user-menu").style.height="250px";
+    document.getElementById("hidden-user-menu").style.width="250px";
+    document.getElementById("header").style.opacity="0.5"
+    
+    
+}
+
+function FecharNav() {
+    document.getElementById("hidden-user-menu").style.width="0";
+    document.getElementById("hidden-user-menu").style.height="0";
+
+}
