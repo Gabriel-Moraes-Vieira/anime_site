32e02b72a97b6dd014344e1435eca2050808d6e1
Gabriel-Moraes-Vieira <gabrieljooj766@gmail.com>
2023-06-16 08:16:04 -0300

    header-update

--- site_anime_projeto/static/js/scripts.js
+++ site_anime_projeto/static/js/scripts.js
@@ -1,13 +1,25 @@
+function ShowOrHide(){
+    if (document.getElementById('genres-list').style.display == "none"){
+    document.getElementById('genres-list').style.display = "block";
+    document.getElementById('main').style.opacity = '0.5'
+    } else{
+        (document.getElementById('genres-list').style.display = "none")
+        document.getElementById('main').style.opacity = '1'
+    }
+};
+
 function AbrirNav(){
     document.getElementById("hidden-user-menu").style.height="250px";
     document.getElementById("hidden-user-menu").style.width="250px";
-    document.getElementById("main-opacity").style.opacity="0.3"
+    document.getElementById("main").style.opacity="0.5"
     
     
-}
+};
 
 function FecharNav() {
     document.getElementById("hidden-user-menu").style.width="0";
     document.getElementById("hidden-user-menu").style.height="0";
-    document.getElementById("main-opacity").style.opacity="1"
-}
+    document.getElementById("main").style.opacity="1"
+};
+
+
