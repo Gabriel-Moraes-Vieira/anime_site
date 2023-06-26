ff26124e49f229902b13299363fcd2a895c07569
Gabriel-Moraes-Vieira <gabrieljooj766@gmail.com>
2023-06-26 08:35:12 -0300

    models-animes-update1

--- animes/admin.py
+++ animes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Animes
 
-# Register your models here.
+class ListandoAnimes(admin.ModelAdmin):
+    list_display = ('id','nome_anime','tags_genero','publicada')
+    list_editable = ('publicada',)
+    search_fields = ('nome_anime',)
+    list_filter = ('nome_anime',)
+
+admin.site.register(Animes, ListandoAnimes)
--- animes/models.py
+++ animes/models.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 class Animes(models.Model):
     pessoa = models.ForeignKey(User, on_delete=models.CASCADE)
     nome_anime = models.CharField(max_length=100)
+    nomes_alternativos = models.CharField(max_length=150)
+    tags_genero = models.CharField(max_length=200)
     anime_sinopse = models.TextField()
     imagem_anime = models.ImageField(upload_to='Imagens/%d/%m/%Y/', blank=True)
-    links = 
+    links =  models.URLField(max_length=255)
     lancamentos = models.BooleanField(default=False)
     publicada = models.BooleanField(default=False)
